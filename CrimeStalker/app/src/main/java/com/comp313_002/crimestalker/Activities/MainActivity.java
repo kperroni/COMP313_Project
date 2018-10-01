324056f38a31ebb6fe7557076d07ca597c88658e
whatdadeuce <whatdadeuce@users.noreply.github.com>
2018-10-01 18:14:39 -0400

    Migrate MainActivity, RegisterUserActivity, ReportCrimeActivity, Crime class and User class

---
+++ CrimeStalker/app/src/main/java/com/comp313_002/crimestalker/Activities/MainActivity.java
@@ -0,0 +1,139 @@
+package com.comp313_002.crimestalker.Activities;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.comp313_002.crimestalker.Classes.Crime;
+import com.comp313_002.crimestalker.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.INTERNET};
+    public static View.OnClickListener myOnClickListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[0]) == PackageManager.PERMISSION_DENIED ||
+                    ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[1]) == PackageManager.PERMISSION_DENIED ||
+                    ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[2]) == PackageManager.PERMISSION_DENIED) {
+                this.requestPermissions( permissions, 1); }
+        myOnClickListener = new MyOnClickListener(this);
+    }
+
+    public void launchRegisterUser(View v) {
+        startActivity(new Intent(this, RegisterUserActivity.class));
+    }
+
+    public void loginUser(View v) {
+        EditText email = findViewById(R.id.editTextMainLoginEmail);
+        EditText password = findViewById(R.id.editTextMainLoginPassword);
+        FirebaseAuth firebase = FirebaseAuth.getInstance();
+        final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+        dialog.setMessage("Hold on...");
+        dialog.show();
+        firebase.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        dialog.dismiss();
+                        if (!task.isSuccessful()) {
+                            // there was an error
+                            Toast.makeText(MainActivity.this, "Authentication failed!", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication successful!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void firebaseTest() {
+        DatabaseReference firedbCrimes = FirebaseDatabase.getInstance().getReference("crimes");
+        DatabaseReference crimes;
+        crimes = FirebaseDatabase.getInstance().getReference();
+        // Creating a crime
+        Crime crime = new Crime("Another crime", "This is another new crime", 2);
+        crimes.child("crimes").push().setValue(crime);
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                for (DataSnapshot crimes : dataSnapshot.getChildren()) {
+                    Crime aCrime = crimes.getValue(Crime.class);
+                    Log.d("My app", "Getting data: "+aCrime.getDescription());
+                }
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+
+        firedbCrimes.addValueEventListener(postListener);
+        FirebaseAuth firebase = FirebaseAuth.getInstance();
+
+
+        firebase.createUserWithEmailAndPassword("k@e.com", "123456").
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            // user registered successfully
+                            // adding profile here
+                            Toast.makeText(MainActivity.this, "User registered", Toast.LENGTH_LONG);
+                        }
+                    }
+                });
+    }
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+
+    }
+    //To Test ReportCrimeActivity
+    public void testActivity_Clicked(View view)
+    {
+        Intent intent = new Intent(this, ReportCrimeActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ CrimeStalker/app/src/main/java/com/comp313_002/crimestalker/Classes/Crime.java
@@ -0,0 +1,120 @@
+package com.comp313_002.crimestalker.Classes;
+
+import java.util.ArrayList;
+
+public class Crime {
+
+    private String title;
+    private String description;
+    private String type;
+    private ArrayList<String> comments;
+    private float latitude;
+    private float longitude;
+    private int witnesses;
+    private boolean policeReported;
+    private String timeStamp;
+    private String photoUrl;
+    private boolean twitterShared;
+
+    // Default constructor
+    public Crime() {
+
+    }
+
+    // Constructor
+    public Crime(String title, String description, int urgency) {
+        this.title = title;
+        this.description = description;
+        comments = new ArrayList<>();
+    }
+    public void addComments(String comment)
+    {
+        comments.add(comment);
+    }
+
+    public String[] getComments()
+    {
+        return (String[])comments.toArray();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getWitnesses() {
+        return witnesses;
+    }
+
+    public void setWitnesses(int witnesses) {
+        this.witnesses = witnesses;
+    }
+
+    public boolean isPoliceReported() {
+        return policeReported;
+    }
+
+    public void setPoliceReported(boolean policeReported) {
+        this.policeReported = policeReported;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public boolean isTwitterShared() {
+        return twitterShared;
+    }
+
+    public void setTwitterShared(boolean twitterShared) {
+        this.twitterShared = twitterShared;
+    }
+
+}
