e6418ee723bdd8eefe9b7f39655cde5e5d0eb885
Manoel Britto <manoelbritto@gmail.com>
2018-10-03 19:23:36 -0400

    adjust read crime

--- CrimeStalker/app/src/main/java/com/comp313_002/crimestalker/Activities/ReadCrime.java
+++ CrimeStalker/app/src/main/java/com/comp313_002/crimestalker/Activities/ReadCrime.java
@@ -1,10 +1,13 @@
-package com.comp313_002.crimestalker;
+package com.comp313_002.crimestalker.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.comp313_002.crimestalker.R;
+
 public class ReadCrime extends AppCompatActivity {
 
+    var database = firebase.database();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
