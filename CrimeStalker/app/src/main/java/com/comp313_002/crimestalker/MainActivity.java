f0006370660da91995f29ef45da55d4e6280afdd
gera <grcg0@yahoo.com>
2018-10-13 17:48:30 -0400

    Implements Twitter.

--- CrimeStalker/app/src/main/java/com/comp313_002/crimestalker/MainActivity.java
+++ CrimeStalker/app/src/main/java/com/comp313_002/crimestalker/MainActivity.java
@@ -1,86 +1,47 @@
 package com.comp313_002.crimestalker;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+
+import java.io.File;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends Activity {
 
 
-    private static RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-    private static RecyclerView recyclerView;
     private static ArrayList<DataModel> data;
-    static View.OnClickListener myOnClickListener;
     private static ArrayList<Integer> removedItems;
 
+    private TwitterLoginButton twitterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
-
-        myOnClickListener = new MyOnClickListener(this);
-
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        recyclerView.setHasFixedSize(true);
-
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        data = new ArrayList<DataModel>();
-        for (int i = 0; i < MyData.nameArray.length; i++) {
-            data.add(new DataModel(
-                    MyData.nameArray[i],
-                    MyData.versionArray[i],
-                    MyData.id_[i],
-                    MyData.drawableArray[i]
-            ));
-        }
-
-        removedItems = new ArrayList<Integer>();
-
-        adapter = new CustomAdapter(data);
-        recyclerView.setAdapter(adapter);
+        setUpTwitterButton();
     }
 
 
-    private static class MyOnClickListener implements View.OnClickListener {
-
-        private final Context context;
-
-        private MyOnClickListener(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        public void onClick(View v) {
-            removeItem(v);
-        }
-
-        private void removeItem(View v) {
-            int selectedItemPosition = recyclerView.getChildPosition(v);
-            RecyclerView.ViewHolder viewHolder
-                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
-            TextView textViewName
-                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
-            String selectedName = (String) textViewName.getText();
-            int selectedItemId = -1;
-            for (int i = 0; i < MyData.nameArray.length; i++) {
-                if (selectedName.equals(MyData.nameArray[i])) {
-                    selectedItemId = MyData.id_[i];
-                }
-            }
-            removedItems.add(selectedItemId);
-            data.remove(selectedItemPosition);
-            adapter.notifyItemRemoved(selectedItemPosition);
-        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterButton.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -90,29 +51,40 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if (item.getItemId() == R.id.add_item) {
-           //check if any items to add
-            if (removedItems.size() != 0) {
-                addRemovedItemToList();
-            } else {
-                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
+    private void setUpTwitterButton() {
+        twitterButton = (TwitterLoginButton) findViewById(R.id.twitter_button);
+        twitterButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Toast.makeText(getApplicationContext(),
+                        getResources().getString(R.string.app_name),
+                        Toast.LENGTH_SHORT).show();
+
+                setUpViewsForTweetComposer();
             }
-        }
-        return true;
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(getApplicationContext(),
+                        getResources().getString(R.string.app_name),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    private void addRemovedItemToList() {
-        int addItemAtListPosition = 3;
-        data.add(addItemAtListPosition, new DataModel(
-                MyData.nameArray[removedItems.get(0)],
-                MyData.versionArray[removedItems.get(0)],
-                MyData.id_[removedItems.get(0)],
-                MyData.drawableArray[removedItems.get(0)]
-        ));
-        adapter.notifyItemInserted(addItemAtListPosition);
-        removedItems.remove(0);
+    private void setUpViewsForTweetComposer() {
+        TweetComposer.Builder builder = new TweetComposer.Builder(this)
+                .text("Just setting up my Fabric!");
+        builder.show();
+    }
+
+
+    private void createInstagramIntent(String type, String mediaPath) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType(type);
+        File media = new File(mediaPath);
+        Uri uri = Uri.fromFile(media);
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(share, getString(R.string.share_to)));
     }
 }
