d626422e44f5667ecf9a5dc19b8cb955d0a36b0d
grcg <grcg0@yahoo.com>
2018-12-03 11:28:31 -0500

    Refactoring of the Twitter post feature.

--- CrimeStalker/app/src/main/java/com/comp313_002/crimestalker/Activities/CommentCrimeActivity.java
+++ CrimeStalker/app/src/main/java/com/comp313_002/crimestalker/Activities/CommentCrimeActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
 import java.util.ArrayList;
@@ -31,13 +32,15 @@ public class CommentCrimeActivity extends AppCompatActivity {
     private FirebaseDatabase database2 = FirebaseDatabase.getInstance();
     private DatabaseReference myRef2 = database2.getReference("crimes/CrimeReports");
 
-    private ListView listViewCrime ;
+    private ListView listViewCrime;
     //List<Crime> crimeList;
     String key;
     Crime crime;
     int maxValue = 0;
     String valueInitial;
     private Context context;
+    private TwitterLoginButton twitterButton;
+    private Button btnPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +48,13 @@ public class CommentCrimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_comment_crime);
         context = this;
         // listen all components
-        listViewCrime = (ListView)findViewById(R.id.listComments);
+        listViewCrime = (ListView) findViewById(R.id.listComments);
         final TextView title = (TextView) findViewById(R.id.textTitle);
         final TextView desc = (TextView) findViewById(R.id.textDesc);
         final TextView oldComment = (TextView) findViewById(R.id.textOldComments);
         final Button btnBack = (Button) findViewById(R.id.buttonBack);
         final Button btnSave = (Button) findViewById(R.id.buttonSave);
-        final Button btnPost = (Button) findViewById(R.id.buttonPost);
+        btnPost = (Button) findViewById(R.id.buttonPost);
         final EditText comment = (EditText) findViewById(R.id.editText);
         // getting the value key from the previous activity ReadCrime
         key = getIntent().getExtras().getString("key");
@@ -63,50 +66,47 @@ public class CommentCrimeActivity extends AppCompatActivity {
 
                 crime = dataSnapshot.getValue(Crime.class);
                 int size = crime.getComments().size();
-                int countRealsize=0;
+                int countRealsize = 0;
                 List value = new ArrayList<>();
                 //this is to fix the real number of the list with the correct component
-                for (int i=0 ; i<crime.getComments().size();i++){
-                    try{
+                for (int i = 0; i < crime.getComments().size(); i++) {
+                    try {
 
-                        if (!crime.getComments().get(i).toString().isEmpty()){
+                        if (!crime.getComments().get(i).toString().isEmpty()) {
 
-                            value.add(countRealsize,crime.getComments().get(i).toString());
-                            countRealsize ++;
+                            value.add(countRealsize, crime.getComments().get(i).toString());
+                            countRealsize++;
                         }
-                    }
-                    catch (Exception e){
-                      continue;
+                    } catch (Exception e) {
+                        continue;
                     }
                 }
 
                 title.setText(crime.getTitle());
                 desc.setText(crime.getDescription());
                 // continue if there are values found in firebase, then in this case the value can be deleted
-                if (value.size()>0){
+                if (value.size() > 0) {
                     maxValue = value.size(); //set the max value
                     oldComment.setText("All Comments: (click to delete)");
-                    ArrayAdapter arrayAdapter = new ArrayAdapter(CommentCrimeActivity.this, android.R.layout.simple_list_item_1,value);
+                    ArrayAdapter arrayAdapter = new ArrayAdapter(CommentCrimeActivity.this, android.R.layout.simple_list_item_1, value);
                     listViewCrime.setAdapter(arrayAdapter);
                     listViewCrime.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
 
                             valueInitial = parent.getItemAtPosition(pos).toString();
-                            for (int i=0 ; i<crime.getComments().size();i++){
-                                try{
+                            for (int i = 0; i < crime.getComments().size(); i++) {
+                                try {
 
-                                    if (crime.getComments().get(i).toString().equals(valueInitial)){
-                                        String internal= String.valueOf(i);
-                                        if (i>0) {
+                                    if (crime.getComments().get(i).toString().equals(valueInitial)) {
+                                        String internal = String.valueOf(i);
+                                        if (i > 0) {
                                             myRef2.child(key).child("comments").child(internal).removeValue();
-                                        }
-                                        else {
+                                        } else {
                                             Toast.makeText(CommentCrimeActivity.this, "This comment cannot be deleted, try anoter!", Toast.LENGTH_LONG).show();
                                         }
                                     }
-                                }
-                                catch (Exception e){
+                                } catch (Exception e) {
                                     continue;
                                 }
                             }
@@ -127,14 +127,24 @@ public class CommentCrimeActivity extends AppCompatActivity {
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(!Strings.isEmptyOrWhitespace(comment.getText().toString())){
+                if (!Strings.isEmptyOrWhitespace(comment.getText().toString())) {
                     String internalValue = String.valueOf(getMaxValue());//define the correct value to be include, otherwise it will leave the firebase to create its owns key
                     myRef2.child(key).child("comments").child(internalValue).setValue(comment.getText().toString());
                     comment.setText("");
                 }
             }
         });
-    //send value to Tweet
+        setUpTwitterButton();
+        //send value to Tweet
+        /**/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void setUpTwitterButton() {
         btnPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -147,28 +157,29 @@ public class CommentCrimeActivity extends AppCompatActivity {
                 builder.show();
             }
         });
-    }
-    //getting the max value to be used correctly into the listview
-    public int getMaxValue (){
-        int max = 0;
-        for (int i=0 ; i<crime.getComments().size();i++){
-            try{
 
-                if (!crime.getComments().get(i).toString().isEmpty()){
-                    String internal= String.valueOf(i);
-                    if (max < i ){
+    }
+
+    //getting the max value to be used correctly into the listview
+    public int getMaxValue() {
+        int max = 0;
+        for (int i = 0; i < crime.getComments().size(); i++) {
+            try {
+
+                if (!crime.getComments().get(i).toString().isEmpty()) {
+                    String internal = String.valueOf(i);
+                    if (max < i) {
                         max = i;
                     }
-                    if (max ==0){
-                        max ++;
+                    if (max == 0) {
+                        max++;
                     }
                 }
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 continue;
             }
         }
-        return max ;
+        return max;
     }
 
 }
